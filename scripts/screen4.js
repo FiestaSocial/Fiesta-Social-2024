86dbcfd3f90dc4a1d642dfd80e9e140773b1abd8
FIESTA SOCIAL <131306524+FiestaSocial@users.noreply.github.com>
2024-09-28 20:33:06 -0300

    Update screen4.js

--- scripts/screen4.js
+++ scripts/screen4.js
@@ -1,18 +1,1 @@
-// screen4.js
-function loadScreen4() {
-    document.getElementById('screen4').innerHTML = `
-        <h2>Selección de Mesa</h2>
-        <!-- Mesa selection and visualization go here -->
-        <div class="navigation-buttons">
-            <button onclick="prevScreen(3)">Anterior</button>
-            <button onclick="submitForm()">Enviar</button>
-        </div>
-        <div>
-            <h3>Invitados Registrados en esta Mesa:</h3>
-            <div id="guestList"></div>
-        </div>
-    `;
-}
-
-loadScreen4();
 
