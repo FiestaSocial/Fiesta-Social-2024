c84683a4795e5dc0704c0af0fbd48676032c3e0f
FIESTA SOCIAL <131306524+FiestaSocial@users.noreply.github.com>
2024-09-28 20:48:22 -0300

    Update screen1.js

--- scripts/screen1.js
+++ scripts/screen1.js
@@ -1,13 +1,1 @@
-// screen1.js
-function loadScreen1() {
-    document.getElementById('screen1').innerHTML = `
-        <h1>Bienvenido al asistente web para la Fiesta Social 2024.</h1>
-        <p>¡Estamos encantados de tenerte aquí!</p>
-        <img src="Imagen de bienvenida.png" alt="Imagen de bienvenida" style="max-width: 100%; height: auto; border-radius: 10px;">
-        <div class="navigation-buttons">
-            <button onclick="nextScreen(2)">Iniciar</button>
-        </div>
-    `;
-}
 
-loadScreen1();
