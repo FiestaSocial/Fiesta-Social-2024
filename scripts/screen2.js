5af7cefc5e1e72169ebb7aed224168ac1c55d17d
FIESTA SOCIAL <131306524+FiestaSocial@users.noreply.github.com>
2024-09-30 14:21:18 -0300

    Delete scripts directory

--- scripts/screen2.js
+++
@@ -1,9 +0,0 @@
-function toggleAcompanante() {
-    let acompanante = document.getElementById("acompanante").value;
-    let acompananteCantidad = document.getElementById("acompananteCantidad");
-    if (acompanante === "si") {
-        acompananteCantidad.style.display = "block";
-    } else {
-        acompananteCantidad.style.display = "none";
-    }
-}
